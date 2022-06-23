2ce34bc0a4d3e6591833e7c47e0c065cdce97e3e
Jamal Mughal <mirzajamaluddin44@gmail.com>
2022-06-23 14:04:57 +0500

    Seventh Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,19 @@ class UsersController<ApplicationController
          render 'new'
          end
     end
-end
+    def edit
+         @user=User.find(params[:id])
+    end
+    def update
+        @user=User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "User was updated successfully"
+            redirect_to articles_path
+           else
+            render 'edit'
+           end
+    end
+    end
 private
 def user_params
     params.require(:user).permit(:username, :email, :password)
